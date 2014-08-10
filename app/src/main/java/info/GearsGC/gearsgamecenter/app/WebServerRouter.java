4155304f4aace4c89f317e6cbf662b1005243251
Greg Zhou <bottlezz2002@gmail.com>
2014-08-10 14:30:25 -0700

    commit changes

--- app/src/main/java/info/GearsGC/gearsgamecenter/app/WebServerRouter.java
+++ app/src/main/java/info/GearsGC/gearsgamecenter/app/WebServerRouter.java
@@ -1,7 +1,33 @@
 package info.GearsGC.gearsgamecenter.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import info.gearsgc.webserver.GcUrlRouter;
+
 /**
  * Created by gregoryz on 2014-08-10.
  */
-public class WebServerRouter {
+public class WebServerRouter implements GcUrlRouter {
+    @Override
+    public String RoutePath(String path) {
+        String lowerPath=path.toLowerCase();
+
+        List<String> apps=AssetAppList();
+        for(String app : apps){
+            if(lowerPath.endsWith(app+'/')){
+                path+="index.html";
+            }
+        }
+        return path;
+    }
+
+    @Override
+    public List<String> AssetAppList() {
+        List<String> apps= new ArrayList<String>();
+        apps.add("whois");
+        apps.add("filemanager");
+
+        return apps;
+    }
 }
